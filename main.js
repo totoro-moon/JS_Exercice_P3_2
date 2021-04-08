875e111cced8d2dcc9d91380af89d0152954c269
FLAVIE <flavie@MacBook-Pro-de-FLAVIE.local>
2021-04-08 13:58:02 +0200

    JS_Exercice_P3_2

---
+++ main.js
@@ -0,0 +1,21 @@
+// fonction qui affiche au clic la div (onclick dans le html)
+// function DisplayDiv() {
+//    document.getElementById("myDIV").style.display = "block";
+// }
+// fonction qui affiche au cache au clic la div (onclick dans le html)
+// function HideDiv() {
+//    document.getElementById("myDIV").style.display = "none";
+// }
+
+function showAndHideText(mytext) {
+   if (mytext == 1) {
+      document.getElementById("myDIV").style.visibility = "visible";
+   } else if (mytext == 2) {
+      document.getElementById("myDIV").style.visibility = "hidden";
+   }
+}
+
+
+
+
+
